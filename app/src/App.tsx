1656484764829ec96bc8d79e1fe716b109e44c3a
joaoMiraya <joaomiraya01@gmail.com>
2025-12-01 22:58:38 -0300

    feat(app): authentication finished

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Outlet } from "react-router"
+import { Container } from "./components/partials/Container"
+import { Header } from "./components/partials/Header"
+import { Footer } from "./components/partials/Footer"
 
 
 function App() {
 
+const queryClient = new QueryClient()
+
   return (
-    <>
-    <p>aaaaaaaaaaaaaau</p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Header />
+      <Container >
+        <Outlet/>
+      </Container>
+      <Footer />
+    </QueryClientProvider>
   )
 }
 
